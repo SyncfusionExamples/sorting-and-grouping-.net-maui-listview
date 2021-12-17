ebd87877099ecb15a591781aac64945d61b16817
Rasika Palanisamy <rasika.palanisamy@gmail.com>
2021-12-17 15:45:09 +0530

    MAUI-832: Added Sorting with grouping by year sample.

---
+++ ListViewMaui/ListViewMaui/Model/Contacts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ListViewMaui
+{
+    public class Contacts : INotifyPropertyChanged
+    {
+        private string contactName;
+        private DateTimeOffset dateOfBirth;
+
+        public Contacts(string name, DateTimeOffset number)
+        {
+            contactName = name;
+            dateOfBirth = number;
+        }
+        public Contacts()
+        {
+
+        }
+        public string ContactName
+        {
+            get { return contactName; }
+            set
+            {
+                if (contactName != value)
+                {
+                    contactName = value;
+                    this.RaisedOnPropertyChanged("ContactName");
+                }
+            }
+        }
+        public DateTimeOffset DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set
+            {
+                if (dateOfBirth != value)
+                {
+                    dateOfBirth = value;
+                    this.RaisedOnPropertyChanged("DateOfBirth");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+    }
+}
---
+++ ListViewMaui/ListViewMaui/ViewModel/ContactsViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ListViewMaui
+{
+    public class ContactsViewModel
+    {
+        #region Properties
+        public ObservableCollection<Contacts> Items { get; set; }
+        #endregion
+
+        #region Constructor
+        public ContactsViewModel()
+        {
+            Items = new ObservableCollection<Contacts>();
+            var date = DateTimeOffset.Now;
+            Random r = new Random();
+            for (int i = 0; i < ContactNames.Count() - 1; i++)
+            {
+                var contact = new Contacts(ContactNames[i], date.AddDays(r.Next(-30, 30)).AddMonths(r.Next(0, 12)).AddYears(r.Next(-5, 0)));
+                Items.Add(contact);
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        string[] ContactNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Michael",
+            "Oscar",
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson",
+            "Mason",
+            "Liam",
+            "Jacob",
+            "Jayden",
+            "Ethan",
+            "Noah",
+            "Lucas",
+            "Logan",
+            "Caleb",
+            "Caden",
+            "Jack",
+            "Ryan",
+            "Connor",
+            "Michael",
+            "Elijah",
+            "Brayden",
+            "Benjamin",
+            "Nicholas",
+            "Alexander",
+            "William",
+            "Matthew",
+            "James",
+            "Landon",
+            "Nathan",
+            "Dylan",
+            "Evan",
+            "Luke",
+            "Andrew",
+            "Gabriel",
+            "Gavin",
+            "Joshua",
+            "Parker"
+        };
+
+        #endregion
+    }
+}
